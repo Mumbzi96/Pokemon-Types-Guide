c01abfd425c5afeb89e703fdee2bd2572870a5a5
Omar Hammoud <omar.s.hammoud@outlook.com>
2022-06-11 15:20:10 +0300

    Initial setup

---
+++ index.js
@@ -0,0 +1,13 @@
+// These lines are essentially including express in this script.
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(80, () => {
+  console.log(`Example app`)
+})

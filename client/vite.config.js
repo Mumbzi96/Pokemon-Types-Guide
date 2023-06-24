75721e9d39882ef3bb4507d57178685d487c2369
Omar Hammoud <omar.s.hammoud@outlook.com>
2023-06-24 19:19:07 +0300

    updated port

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		port: 5000,
+		port: 443,
 		proxy: {
 			"/api": "http://localhost:7777",
 		},
